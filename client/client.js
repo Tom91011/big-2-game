a7b6d3fabc2e53972e22e36116b53ea6c0c30d22
Andrew Bullock <trullock@gmail.com>
2021-12-23 20:47:25 +0000

    improved hardcoded demo code

--- client/client.js
+++ client/client.js
@@ -6,8 +6,9 @@ var socket = new WebSocket("ws://localhost:8080",);
 socket.onopen = async function() {
 	var view = new View(document);
 	var controller = new ClientController(socket, view);
+
+	// demo hack to get things started
 	let game = await controller.createGame("1234", "Test game")
 	controller.deal(game.id);
-	console.log('game created')
 };
 
