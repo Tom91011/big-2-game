fc3825d08383a6f9b11f9ae860a33e38fd9f6b90
Tom91011 <tomnightingale@hotmail.com>
2022-01-19 12:59:29 +0000

    Add all names to all views of the game

--- server/game.js
+++ server/game.js
@@ -103,7 +103,7 @@ export default class Game
 	/// Returns an array of hand-views for each player.
 	///  Your own hand contains the cards, others' hands contains just the number of cards
 	#getPlayersHands()
-	{
+	{	;
 		let playersArray = Object.values(this.#players)
 		let playersHands = [];
 		for(var p = 0; p < playersArray.length; p++)
@@ -118,9 +118,10 @@ export default class Game
 						cards: player.cards,
 						currentPlayer: this.#currentPlayerIndex == i
 					};
-
+					
 				// someone elses hand
 				return {
+					playerName: player.name,
 					playerId: player.id,
 					cardsRemaining: player.cards.length,
 					currentPlayer: this.#currentPlayerIndex == i
