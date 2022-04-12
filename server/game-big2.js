235b3a0ebc84a49ecc5f453b42b2ecddf452a778
Andrew Bullock <trullock@gmail.com>
2022-04-12 18:22:16 +0100

    added card images

---
+++ server/game-big2.js
@@ -0,0 +1,12 @@
+export default class GameBig2 extends Game
+{
+	constructor(name, id)
+	{
+		super(name, id);
+	}
+
+	playHand(playerId, cards)
+	{
+		return super.playHand(playerId, cards);
+	}
+}
